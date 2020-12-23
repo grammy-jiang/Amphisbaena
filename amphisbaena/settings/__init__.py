f6152553702210594d28335fc8576be70c633f19
Grammy Jiang <grammy.jiang@gmail.com>
2020-12-24 01:14:29 +1100

    Add settings module and its test cases

---
+++ amphisbaena/settings/__init__.py
@@ -0,0 +1,229 @@
+"""
+Settings
+"""
+
+from __future__ import annotations
+
+from collections import namedtuple
+from collections.abc import MutableMapping
+from contextlib import contextmanager
+from importlib import import_module
+from types import ModuleType
+from typing import Any, Dict, Generator, Iterator, Mapping, Union
+
+# The pair of priority and priority_value
+PRIORITIES: Dict[str, int] = {
+    "default": 0,
+    "project": 20,
+    "env": 40,
+    "cmd": 60,
+}
+
+Setting = namedtuple("Setting", ["priority", "priority_value", "value"])
+
+
+class SettingsException(Exception):
+    """
+    The base exception
+    """
+
+
+class SettingsFrozenException(SettingsException):
+    """
+    The exception when modify a frozen settings instance
+    """
+
+
+class SettingsLowPriorityException(SettingsException):
+    """
+    The exception when modify a setting with a lower priority
+    """
+
+
+class BaseSettings(MutableMapping):
+    """
+    base settings class
+    """
+
+    class FrozenCheck:  # pylint: disable = too-few-public-methods
+        """
+        A decorator for Settings frozen status check
+        """
+
+        def __call__(self, method):
+            def frozen_check(settings: BaseSettings, *args, **kwargs):
+                if settings.is_frozen():
+                    raise SettingsFrozenException
+                return method(settings, *args, **kwargs)
+
+            return frozen_check
+
+    frozen_check = FrozenCheck()
+
+    def __init__(self, settings: Mapping = None, priority: str = "project"):
+        """
+
+        :param settings:
+        :type settings: Mapping
+        :param priority:
+        :type priority: str
+        """
+        self._priority = priority
+
+        self._data: Dict[str, Setting] = {}
+
+        self._frozen: bool = False
+
+        if settings:
+            self.update(settings)
+
+        self._frozen = True
+
+    def is_frozen(self) -> bool:
+        """
+        check this settings class frozen or not
+        :return:
+        """
+        return self._frozen
+
+    @contextmanager
+    def unfreeze(self, priority: str = "project") -> Generator:
+        """
+        A context manager to unfreeze this instance and keep the previous frozen
+        status
+        """
+        _priority: str
+        _priority, self._priority = self._priority, priority
+
+        status: bool
+        status, self._frozen = self._frozen, False
+
+        try:
+            yield self
+        finally:
+            self._priority = _priority
+            self._frozen = status
+
+    # ---- abstract methods of MutableMapping ---------------------------------
+
+    @frozen_check
+    def __setitem__(self, k: str, v: Any) -> None:
+        """
+
+        :param k:
+        :type k: str
+        :param v:
+        :type v: Any
+        :return:
+        :rtype: None
+        """
+        setting: Setting = Setting(
+            priority=self._priority,
+            priority_value=PRIORITIES[self._priority],
+            value=v,
+        )
+        if k in self:
+            _v = self._data[k]
+            if PRIORITIES[self._priority] < _v.priority_value:
+                raise SettingsLowPriorityException
+
+        self._data[k] = setting
+
+    @frozen_check
+    def __delitem__(self, k: str) -> None:
+        """
+
+        :param k:
+        :type k: str
+        :return:
+        :rtype: None
+        """
+        del self._data[k]
+
+    def __getitem__(self, k: str) -> Any:
+        """
+
+        :param k:
+        :type k: str
+        :return:
+        :rtype: Any
+        """
+        return self._data[k].value
+
+    def __len__(self) -> int:
+        """
+
+        :return:
+        :rtype: int
+        """
+        return len(self._data)
+
+    def __iter__(self) -> Iterator[str]:
+        """
+
+        :return:
+        :rtype: Iterator[str]
+        """
+        return iter(self._data)
+
+    def __contains__(self, k: str) -> bool:  # type: ignore
+        """
+
+        :param k:
+        :type k: str
+        :return:
+        :rtype: bool
+        """
+        return k in self._data
+
+
+class Settings(BaseSettings):  # pylint: disable=too-many-ancestors
+    """
+    settings class
+    """
+
+    def __init__(
+        self,
+        settings: Mapping = None,
+        priority: str = "project",
+        load_default: bool = True,
+    ):
+        """
+
+        :param settings:
+        :type settings: Mapping
+        :param priority:
+        :type priority: str
+        :param load_default:
+        :type load_default: bool
+        """
+        super().__init__(settings, priority)
+
+        if load_default:
+            with self.unfreeze(priority="default") as settings_:
+                settings_.load_module(  # pylint: disable=no-member
+                    f"{self.__module__}.default_settings"
+                )
+
+    def load_module(self, module: Union[ModuleType, str]) -> None:
+        """
+
+        :param module:
+        :type module: Union[ModuleType, str]
+        :return:
+        :rtype: None
+        """
+        if isinstance(module, str):
+            module = import_module(module)
+
+        for key in dir(module):
+            if key.isupper():
+                self[key] = getattr(module, key)
+
+    def copy_to_dict(self) -> Dict[str, Any]:
+        """
+
+        :return:
+        :rtype: Dict[str, Any]
+        """
+        return dict(self.items())
---
+++ tests/settings/test_settings.py
@@ -0,0 +1,195 @@
+"""
+Test BaseSettings class
+"""
+from collections.abc import Iterable
+from types import ModuleType
+from unittest.case import TestCase
+
+from amphisbaena.settings import (
+    BaseSettings,
+    Setting,
+    Settings,
+    SettingsFrozenException,
+    SettingsLowPriorityException,
+)
+
+
+class BaseSettingsTest(TestCase):
+    """
+    test BaseSettings class
+    """
+
+    def test_init(self) -> None:
+        """
+        test the initialize method
+        :return:
+        :rtype: None
+        """
+
+        settings = BaseSettings()
+        self.assertDictEqual(settings._data, {})  # pylint: disable = protected-access
+
+        settings = BaseSettings(settings={"a": 1, "b": 2})
+        self.assertDictEqual(
+            settings._data,  # pylint: disable = protected-access
+            {
+                "a": Setting(priority="project", priority_value=20, value=1),
+                "b": Setting(priority="project", priority_value=20, value=2),
+            },
+        )
+
+        self.assertTrue(settings.is_frozen())
+        self.assertEqual(
+            settings._priority, "project"  # pylint: disable = protected-access
+        )
+
+    def test_is_frozen(self) -> None:
+        """
+        test the method is_frozen
+        :return:
+        :rtype: None
+        """
+
+        settings = BaseSettings()
+        self.assertTrue(settings.is_frozen())
+
+        with settings.unfreeze() as settings_:
+            self.assertFalse(settings_.is_frozen())
+
+    def test_unfreeze(self) -> None:
+        """
+        test the context manager unfreeze
+        :return:
+        :rtype: None
+        """
+
+        settings = BaseSettings()
+        self.assertTrue(settings.is_frozen())
+        with settings.unfreeze() as settings_:
+            self.assertFalse(settings_.is_frozen())
+        self.assertEqual(
+            settings._priority,  # pylint: disable = protected-access
+            "project",
+        )
+
+        with settings.unfreeze(priority="user") as settings_:
+            self.assertFalse(settings_.is_frozen())
+            self.assertEqual(
+                settings_._priority, "user"  # pylint: disable = protected-access
+            )
+        self.assertEqual(
+            settings._priority, "project"  # pylint: disable = protected-access
+        )
+
+    def test_setitem(self):
+        """
+        test the method of setitem
+        :return:
+        """
+        settings = BaseSettings(settings={"a": 1, "b": 2})
+        with settings.unfreeze() as settings_:
+            settings_["c"] = 3
+        self.assertEqual(settings["c"], 3)
+
+        with self.assertRaises(SettingsFrozenException):
+            settings["c"] = 3
+
+        with self.assertRaises(SettingsLowPriorityException):
+            with settings.unfreeze("default") as settings_:
+                settings_["a"] = 3
+
+    def test_delitem(self):
+        """
+        test the method of del
+        :return:
+        """
+        settings = BaseSettings(settings={"a": 1, "b": 2})
+        with settings.unfreeze() as settings_:
+            del settings_["a"]
+        self.assertNotIn("a", settings)
+
+        with self.assertRaises(SettingsFrozenException):
+            del settings["a"]
+
+    def test_getitem(self):
+        """
+        test the method of getitem
+        :return:
+        """
+        settings = BaseSettings(settings={"a": 1, "b": 2})
+        self.assertEqual(settings["a"], 1)
+
+    def test_len(self):
+        """
+        test the method of len
+        :return:
+        """
+        settings = BaseSettings(settings={"a": 1, "b": 2})
+        self.assertEqual(len(settings), 2)
+
+        settings = BaseSettings()
+        self.assertEqual(len(settings), 0)
+
+    def test_iter(self):
+        """
+        test the method of iter
+        :return:
+        """
+        settings = BaseSettings()
+        self.assertIsInstance(settings, Iterable)
+
+    def test_contains(self):
+        """
+        test the method of contains
+        :return:
+        """
+        settings = BaseSettings(settings={"a": 1, "b": 2})
+        self.assertTrue("a" in settings)
+        self.assertFalse("c" in settings)
+
+
+class SettingsTest(TestCase):
+    """
+    test Settings class
+    """
+
+    def test_update_from_module(self):
+        """
+        test the method of update_from_module
+        :return:
+        """
+        test_module = ModuleType(name="test")
+        setattr(test_module, "A", 1)
+
+        settings = Settings()
+        settings_: Settings
+        with settings.unfreeze() as settings_:
+            settings_.load_module(test_module)  # pylint: disable=no-member
+
+        self.assertIn("A", settings)
+        self.assertEqual(
+            settings._data["A"],  # pylint: disable = protected-access
+            Setting(priority="project", priority_value=20, value=1),
+        )
+
+        # settings = Settings()
+        # settings_: Settings
+        # with settings.unfreeze() as settings_:
+        #     settings_.load_module(  # pylint: disable=no-member
+        #         "amphisbaena.settings.default"
+        #     )
+
+        # self.assertIn("LOG_LEVEL", settings)
+        # self.assertEqual(
+        #     settings._data["LOG_LEVEL"],  # pylint: disable = protected-access
+        #     Setting(priority="project", priority_value=20, value=logging.INFO),
+        # )
+
+    def test_copy_to_dict(self):
+        """
+
+        :return:
+        """
+        settings = Settings(settings={"A": 1, "B": 2}, load_default=False)
+
+        self.assertDictEqual(settings.copy_to_dict(), {"A": 1, "B": 2})
