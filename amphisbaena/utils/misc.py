985ef5b1e91c340ac68a07f253561d032dc7af37
Grammy Jiang <grammy.jiang@gmail.com>
2021-01-02 08:09:32 +1100

    Simplify load_object and its test cases

--- amphisbaena/utils/misc.py
+++ amphisbaena/utils/misc.py
@@ -7,20 +7,14 @@ from typing import Any
 
 
 @lru_cache
-def load_object(path: str, *, type_: str = "class") -> Any:
+def load_object(path: str) -> Any:
     """
 
     :param path:
     :type path: str
-    :param type_:
-    :type type_: str
     :return:
     :rtype: Any
     """
-    if type_ in ("class", "function", "variable"):  # class or function
-        module, name = path.rsplit(".", 1)
-        module_ = import_module(module)
-        return getattr(module_, name)
-
-    if type_ == "module":
-        return import_module(path)
+    module, name = path.rsplit(".", 1)
+    module_ = import_module(module)
+    return getattr(module_, name)
--- tests/utils/test_misc.py
+++ tests/utils/test_misc.py
@@ -20,23 +20,16 @@ class MiscTest(TestCase):
         :return:
         :rtype: None
         """
-        obj = load_object("amphisbaena.settings.Settings")
+        obj = load_object("amphisbaena.settings.Settings")  # class
         self.assertIs(obj, Settings)
 
-        func = load_object("amphisbaena.utils.misc.load_object", type_="function")
+        func = load_object("amphisbaena.utils.misc.load_object")  # func
         self.assertIs(func, load_object)
 
-        var = load_object("amphisbaena.settings.PRIORITIES", type_="variable")
+        var = load_object("amphisbaena.settings.PRIORITIES")  # variable
         self.assertIs(var, PRIORITIES)
 
-    def test_load_object_module(self) -> None:
-        """
-
-        :return:
-        :rtype: None
-        """
-        module = load_object("tests.samples.settings", type_="module")
-
+        module = load_object("tests.samples.settings")  # module
         self.assertIs(module, settings)
 
 
