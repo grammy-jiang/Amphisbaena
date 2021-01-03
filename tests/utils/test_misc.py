3087d90e1406edb2c38353feb5a4ddfa297bb972
Grammy Jiang <grammy.jiang@gmail.com>
2021-01-04 09:50:27 +1100

    Update the test case for load_object

--- tests/utils/test_misc.py
+++ tests/utils/test_misc.py
@@ -24,12 +24,30 @@ class MiscTest(TestCase):
         obj = load_object("amphisbaena.settings.Settings")  # class
         self.assertIs(obj, Settings)
 
+    def test_load_object_func(self) -> None:
+        """
+
+        :return:
+        :rtype: None
+        """
         func = load_object("amphisbaena.utils.misc.load_object")  # func
         self.assertIs(func, load_object)
 
+    def test_load_object_variable(self) -> None:
+        """
+
+        :return:
+        :rtype: None
+        """
         var = load_object("amphisbaena.settings.PRIORITIES")  # variable
         self.assertIs(var, PRIORITIES)
 
+    def test_load_object_module(self) -> None:
+        """
+
+        :return:
+        :rtype: None
+        """
         module = load_object("tests.samples.settings")  # module
         self.assertIs(module, settings)
 
