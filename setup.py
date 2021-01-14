680853a0e55184824aff419cdc4281e7d3db568e
Grammy Jiang <grammy.jiang@gmail.com>
2021-01-14 23:33:42 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 """
 setup.py
 """
-from setuptools import setup
+from setuptools import find_packages, setup
 
 import versioneer
 
 setup(
+    name="Amphisbaena",
+    author="Grammy Jiang",
+    author_email="grammy.jiang@gmail.com",
+    packages=find_packages(),
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass()
+    cmdclass=versioneer.get_cmdclass(),
+    install_requires=[
+        "orjson",
+        "pyyaml",
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "amphisbaena=amphisbaena.__main__:main",
+        ],
+    },
 )
